488273443f3380504fc03d4ad6525d2874524591
grassswwang <grassswwang@tencent.com>
2021-02-28 16:18:49 +0800

    添加一个sayHello的例子

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,11 @@ Java_com_grass_hellondk_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_grass_hellondk_MainActivity_sayHello(JNIEnv *env, jobject thiz) {
+    // TODO: implement sayHello()
+    std::string hello = "sayHello() is working";
+    return env->NewStringUTF(hello.c_str());
+}
